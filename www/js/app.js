5588d33d84b69c627d5c6354078bfba735681c80
Christian Käslin <c.kaeslin@gmx.ch>
2016-08-14 17:14:03 +0200

    add url again

--- www/js/app.js
+++ www/js/app.js
@@ -53,9 +53,9 @@ angular.module('starter', ['ionic'])
 .controller("MainController", function($scope, $rootScope, $state, $stateParams, $pouchDB, $http) {
   $scope.lessons = {};
 
-  /*$http.get("http://192.168.1.107:4984/freshup")
+  $http.get("http://192.168.1.107:4984/freshup")
        .then((data) => $scope.message = data.data)
-       .catch((err) => $scope.message = err);*/
+       .catch((err) => $scope.message = err);
 
   //$pouchDB.startListening();
 
